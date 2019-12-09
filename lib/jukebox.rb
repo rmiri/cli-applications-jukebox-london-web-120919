c14a81c5e185bb5bfa584eb0d4e64af4a9f81843
Renata Freitas <r.miriuk@gmail.com>
2019-12-09 22:03:26 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,16 @@
-# Add your code here
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+def list(songs)
+songs.each_with_index { |song, index|
+  puts "#{index+1}. #{song}"
+}
+end
+
+def exit_jukebox
+  puts 'Goodbye'
+end
